19b40658322fa8b46961912b75b0c27aa87aed82
ivoryzhangwy <ivoryzhangwy@gmail.com>
2024-08-01 17:08:47 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sdl-webui',
+    name='ivoryOS',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    description='Design experiment on any Python-based SDLs.',
+    description='an open-source Python package enabling Self-Driving Labs (SDLs) interoperability',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Ivory Zhang',
@@ -18,8 +18,11 @@ setup(
         "Flask-Session",
         "Flask-SocketIO",
         "Flask-SQLAlchemy",
+        "Flask-WTF",
         "mysqlclient==2.1.1",
         "SQLAlchemy-Utils",
+        "openai",
+        "python-dotenv",
     ],
-    url='https://gitlab.com/heingroup/web_controller'
+    url='https://gitlab.com/heingroup/ivoryos'
 )
